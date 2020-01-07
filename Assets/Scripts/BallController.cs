b73292824b84b15883359cc45d7169c188eb0286
Ivan Mezh <vfeodorovm@yandex.com>
2020-01-08 01:03:18 +0300

    Main mechanics create

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,35 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
+    
+
     public float jumpForceScale;
 
     private Vector2 mousePos;
+    internal int jumpCount;
     
-
-    
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
         if (Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.zero);
+            
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             GetComponent<Rigidbody2D>().AddForce(mousePos, ForceMode2D.Impulse);
         }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        GetComponent<Rigidbody2D>().AddForce(Vector2.zero);
         GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForceScale, ForceMode2D.Impulse);
+        jumpCount++;
+        
     }
 }
---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformScript : MonoBehaviour
+{
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ChangeColor();
+    }
+
+
+    private void ChangeColor()
+    {
+        int r = Random.Range(0, 255);
+        int g = Random.Range(0, 255);
+        int b = Random.Range(0, 255);
+        GetComponent<SpriteRenderer>().color = new Color32((byte)r, (byte)g, (byte)b, 255);
+        
+    }
+}
