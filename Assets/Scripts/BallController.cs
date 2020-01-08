4e13b69eeba151ad302859d4fd99a99f39ed10cf
Ivan Mezh <vfeodorovm@yandex.com>
2020-01-08 04:49:27 +0300

    Add UI and Select Planets

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,31 +4,76 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
-{
-    
-
+{ 
     public float jumpForceScale;
 
+    internal GameObject panel;
+    internal Rigidbody2D ball;
+
+    private GameObject text;
     private Vector2 mousePos;
-    internal int jumpCount;
-    
-    void Update()
+    private int jumpCount = 0;
+
+
+    private void Start()
+    {
+        panel = GameObject.Find("Panel");
+        text = GameObject.Find("CountJumpText");
+        panel.SetActive(false);
+        ball = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
     {
 
         if (Input.GetMouseButtonDown(0))
         {
-            
+
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GetComponent<Rigidbody2D>().AddForce(mousePos, ForceMode2D.Impulse);
+            ball.AddForce(mousePos, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(panel.activeSelf == false)
+            {
+                panel.SetActive(true);
+                Stop(true);
+            }
+            else
+            {
+                panel.SetActive(false);
+                Stop(false);
+            }
+
         }
         
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.zero);
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForceScale, ForceMode2D.Impulse);
         jumpCount++;
-        
+        ball.AddForce(Vector2.zero);
+        ball.AddForce(Vector2.up * jumpForceScale, ForceMode2D.Impulse);
+
+        text.GetComponent<Text>().text = "Jump Count: "+jumpCount.ToString();
+
+
+    }
+
+    virtual internal void Stop(bool state)
+    {
+        if(state == true)
+        {
+            ball.bodyType = RigidbodyType2D.Static;
+        }
+        else
+        {
+            ball.bodyType = RigidbodyType2D.Dynamic;
+            
+        }
+
     }
 }
---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public float g;
+    public new Camera camera;
+    public Color backgroundColor;
+
+    GameObject panel;
+    Rigidbody2D ball;
+    
+    
+    private void Start()
+    {
+
+        panel = GameObject.Find("Panel");
+        ball = GameObject.Find("Ball").GetComponent<Rigidbody2D>();
+    }
+
+    public void StartPlanet()
+    {
+        Physics2D.gravity = new Vector2(0f, g);
+        panel.SetActive(false);
+        Stop(false);
+        camera.backgroundColor = backgroundColor;
+        
+        
+    }
+
+    void Stop(bool state)
+    {
+        if (state == true)
+        {
+            ball.bodyType = RigidbodyType2D.Static;
+        }
+        else
+        {
+            ball.bodyType = RigidbodyType2D.Dynamic;
+
+        }
+    }
+
+    
+
+    
+
+
+
+
+
+}
